a070e08d13963ed94a0790a733fbea0e159f7cc3
Ryan Wilson <inbox@ryanwilson.space>
2024-09-14 15:38:50 -0400

    Add a suggestion

--- src/commands/readLegal.ts
+++ src/commands/readLegal.ts
@@ -1,3 +1,5 @@
 // TODO: Make this command which when provided with `Terms of Service/ToS/Privacy Policy/PP` (case doesn't matter) it will return the respective legal document.
 
 // TODO: This will parse the markdown in the terms directory and get the text from the file (except from the title) and display it in the embed
+
+// Perhaps use https://deno.land/x/markdown_wasm@1.2.2?
